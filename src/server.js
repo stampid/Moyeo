ccd3836d04761ec7737c87acc95db4d19c896917
Jaeyoung Song <sjy920404@naver.com>
2019-07-09 17:04:29 +0900

    set up express server environment and middleware ...ing

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import socketIO from "socket.io";
+import socketController from "./socket.io/socketConroller";
+
+require("dotenv").config();
+
+const app = express();
+const { PORT } = process.env;
+
+app.use(logger("dev"));
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const server = app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+
+const io = socketIO.listen(server);
+
+io.on("connection", socket => socketController(socket));
