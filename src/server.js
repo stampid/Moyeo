4c881df23342e3e4e6e9c470f92381c9775c5bd6
stampid <vashstampid03@gmail.com>
2019-07-10 14:02:30 +0900

    routing process, middleware update and Express server Complete

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import bodyParser from "body-parser";
 import socketIO from "socket.io";
 import socketController from "./socket.io/socketConroller";
 import { sequelize } from "../models";
+import routes from "./routes"; // 분기를 위한 url 모음 파일
+import userRouter from "./routers/userRouter"; // userRouter 파일
 
 // const Sequelize = require("sequelize");
 
@@ -19,6 +21,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(routes.users, userRouter);
+// app.use(routes.rooms);
+// app.use(routes.schedules);
 
 const server = app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
