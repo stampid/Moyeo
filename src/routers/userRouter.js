e9a7ff8180f037051416430fca26b68e2e7fd7ce
Jaeyoung Song <sjy920404@naver.com>
2019-07-13 23:15:50 +0900

    Perfect myRoom List

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,21 @@
 import express from "express";
 import routes from "../routes";
-import { signup, login, logout } from "../controllers/userController";
+import {
+  signup,
+  login,
+  logout,
+  userRooms
+} from "../controllers/userController";
 import { verifyJWT } from "../middleware/JWThelper";
 
 const router = express.Router();
 router.post(routes.login, login); // 로그인
 router.post(routes.signup, signup); // 회원가입
 
-router.use("/", verifyJWT); // 토큰 검증 미들웨어
+// router.use("/", verifyJWT); // 토큰 검증 미들웨어
 
 router.post(routes.logout, logout); // 로그아웃
+
+router.get(routes.userRooms, userRooms); // 내 방 리스트
 
 export default router;
