f2212e58549920c6c5cf97171b5f1d057f4e7328
stampid <vashstampid03@gmail.com>
2019-07-16 15:08:29 +0900

    process.env file fix

--- models/index.js
+++ models/index.js
@@ -15,8 +15,8 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
+    process.env.DATABASE,
+    process.env.DATABASEUSER,
     process.env.DBPASSWORD,
     config
   );
